a8eaa1a51734509f619487c1e2a871aef4b6d20e
danny <dannyjoel@gmail.com>
2021-06-07 22:42:44 -0500

    Acceso al sistema

---
+++ spring-catalogos-api/src/main/java/pe/com/tpp/api/controllers/LoginController.java
@@ -0,0 +1,41 @@
+package pe.com.tpp.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import pe.com.tpp.api.entity.Login;
+import pe.com.tpp.api.entity.Usuarios;
+import pe.com.tpp.api.service.IUsuariosService;
+
+import javax.validation.Valid;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	private IUsuariosService usuarioService;
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@PostMapping("/login")
+	public String verificarAccesos(@Valid Login login, Model model) {
+		
+		model.addAttribute("titulo","Listado de HUs");
+		model.addAttribute("accesos", login);
+		
+		Usuarios usuarios = usuarioService.buscarAccesos(login);
+		
+		if (usuarios != null)
+		{
+			return "listado";
+		}
+		
+		return "login";
+	}
+}
