5e1ba8e31cc2cefb0243df5fc55869dfc013534b
danny <dannyjoel@gmail.com>
2021-06-08 22:49:53 -0500

    Listo

--- spring-catalogos-api/src/main/java/pe/com/tpp/api/controllers/AplicacionController.java
+++ spring-catalogos-api/src/main/java/pe/com/tpp/api/controllers/AplicacionController.java
@@ -6,9 +6,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import pe.com.tpp.api.entity.Historias;
 import pe.com.tpp.api.entity.Login;
 import pe.com.tpp.api.entity.Usuarios;
 import pe.com.tpp.api.service.IUsuariosService;
+
+import java.util.Map;
 
 import javax.validation.Valid;
 
@@ -19,7 +22,8 @@ public class AplicacionController {
 	private IUsuariosService usuarioService;
 	
 	@GetMapping({"/login", "/"})
-	public String login() {
+	public String login(Model model) {
+		model.addAttribute("titulo", "DEV");
 		return "login";
 	}
 	
@@ -40,7 +44,10 @@ public class AplicacionController {
 	}
 	
 	@GetMapping("/registrar")
-	public String HURegistrar() {
+	public String HURegistrar(Map<String, Object> model) {
+		
+		Historias historias = new Historias();
+		model.put("historia", historias);
 		return "historias/registrar";
 	}
 	
